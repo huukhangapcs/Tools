1643ab847b484210099051a17d008aa844f30bd8
Nguyen Khang <44218750+huukhangapcs@users.noreply.github.com>
2021-11-26 15:27:04 +0700

    Create remove_silence.py

---
+++ remove_silence.py
@@ -0,0 +1,28 @@
+from pydub import AudioSegment
+
+
+def detect_leading_silence(sound, silence_threshold=-50.0, chunk_size=10):
+    '''
+    sound is a pydub.AudioSegment
+    silence_threshold in dB
+    chunk_size in ms
+    iterate over chunks until you find the first one with sound
+    '''
+    trim_ms = 0  # ms
+    while sound[trim_ms:trim_ms+chunk_size].dBFS < silence_threshold:
+        trim_ms += chunk_size
+
+    return trim_ms
+
+
+if __name__ == '__main__':
+    import sys
+
+    sound = AudioSegment.from_file(sys.argv[1], format="wav")
+
+    start_trim = detect_leading_silence(sound)
+    end_trim = detect_leading_silence(sound.reverse())
+
+    duration = len(sound)
+    trimmed_sound = sound[start_trim:duration-end_trim]
+    trimmed_sound.export(sys.argv[1], format="wav")
